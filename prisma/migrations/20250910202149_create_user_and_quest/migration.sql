a510304f844a4c9992030e2957982af66862089e
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-09-10 22:10:29 -0300

    initial commit: setup project base

---
+++ prisma/migrations/20250910202149_create_user_and_quest/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "public"."users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."quests" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "goal" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "author_id" TEXT NOT NULL,
+
+    CONSTRAINT "quests_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "quests_slug_key" ON "public"."quests"("slug");
+
+-- AddForeignKey
+ALTER TABLE "public"."quests" ADD CONSTRAINT "quests_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "public"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
